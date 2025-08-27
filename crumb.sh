d5f05a11c533c86403656ab2095430de2144ffa2
jenkins <jenkins@localhost.localdomain>
2025-08-28 01:33:50 +0530

    updated crumb.sh

--- crumb.sh
+++ crumb.sh
@@ -3,4 +3,10 @@
 
 curl \
 -u admin:1167b143ac7bc691547463215ce908a71d `# Replace with your user and API token` \
+-X POST http://jenkins.local:8080/job/jenkins-ansible-db/buildWithParameters?delay=0sec \
+--data-urlencode AGE=23
+
+
+curl \
+-u admin:1167b143ac7bc691547463215ce908a71d `# Replace with your user and API token` \
 -X POST http://jenkins.local:8080/job/job_trigger_using_bash/build?delay=0sec
