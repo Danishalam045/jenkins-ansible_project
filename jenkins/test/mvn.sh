3e60ec16441cf3b83bc99dd18a9d271f81e992d1
jenkins <jenkins@localhost.localdomain>
2025-09-06 12:56:42 +0530

    updated the test script

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,4 +3,6 @@ echo "***************************"
 echo "***** Testing the code ********"
 echo "***************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.11 "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.11 "$@"
